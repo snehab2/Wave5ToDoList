b2d997c20f738d51a01b9f0a8d523e95a0b5b8d5
Sneha Bista <snehab@prodigy.net>
2023-08-15 09:21:05 -0500

    Initial Commit

---
+++ Wave5ToDoList/Wave5ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wave5ToDoListApp.swift
+//  Wave5ToDoList
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Wave5ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
